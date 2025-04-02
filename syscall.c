2a6f94c203d4d361fa3f50b472c92a35516c0846
unbridled-41 <yangzihao_41@outlook.com>
2025-04-02 13:39:32 +0800

    对实验二小的修改

--- syscall.c
+++ syscall.c
@@ -165,7 +165,7 @@ syscall(void)
   num = curproc->tf->eax;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     curproc->tf->eax = syscalls[num]();
-    //cprintf("%s -> %d\n",syscallnames[num] ,syscalls[num]());
+    //cprintf("%s -> %d\n",syscallnames[num] ,syscalls[num]()); //在测试其他的时候将这个注释掉，以便操作
   } else {
     cprintf("%d %s: unknown sys call %d\n",
             curproc->pid, curproc->name, num);
